d45b35ed6152300b35f8a5b9b7e3b9ae6c029a5c
Kanishka Wani <149792494+Kanishka-Wani@users.noreply.github.com>
2026-08-11 08:55:51 +0530

    Use of Java variables

---
+++ Week1/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        String name = "Kanishka";
+        int age = 20;
+        double percentage = 95.20;
+        char grade = 'A';
+        boolean student = true;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + student);
+    }
+}
