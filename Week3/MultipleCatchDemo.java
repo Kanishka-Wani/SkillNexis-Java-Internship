2fb4c406f730bdafea7c0808dc71e790c8b67c20
Kanishka Wani <149792494+Kanishka-Wani@users.noreply.github.com>
2026-09-04 08:20:40 +0530

    Add MultipleCatchDemo to demonstrate exception handling

---
+++ Week3/MultipleCatchDemo.java
@@ -0,0 +1,19 @@
+public class MultipleCatchDemo {
+    public static void main(String[] args) {
+
+        try {
+            int[] arr = {1, 2, 3};
+
+            System.out.println(arr[5]);
+
+            int x = 10 / 0;
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index error!");
+        } catch (ArithmeticException e) {
+            System.out.println("Arithmetic error!");
+        } catch (Exception e) {
+            System.out.println("General exception!");
+        }
+    }
+}
