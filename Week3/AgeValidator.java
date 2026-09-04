cfc3be997b5e9e2c170fb6b0e94d4b719b7514cf
Kanishka Wani <149792494+Kanishka-Wani@users.noreply.github.com>
2026-09-04 08:21:36 +0530

    Add AgeValidator class for age validation
    Implement age validation logic with exception handling.

---
+++ Week3/AgeValidator.java
@@ -0,0 +1,22 @@
+public class AgeValidator {
+
+    static void validate(int age)
+            throws InvalidAgeException {
+
+        if (age < 18) {
+            throw new InvalidAgeException(
+                    "Age must be 18 or above");
+        }
+
+        System.out.println("Valid age");
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            validate(15);
+        } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
