a384940e086a62d8f0a58dab205d58b1701f16f5
Kanishka Wani <149792494+Kanishka-Wani@users.noreply.github.com>
2026-09-04 08:27:14 +0530

    Add DBConnection class for MySQL database connection

---
+++ Week4/CapstoneProject_StudentManagement/DBConnection.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/studentdb";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws Exception {
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        return DriverManager.getConnection(
+                URL,
+                USER,
+                PASSWORD
+        );
+    }
+}
